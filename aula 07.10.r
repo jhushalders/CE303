9980dd0e737585d7cc4a2e2990e73c6df6ff79ae
jhushalders <jhullyashalders@ufpr.br>
2024-10-07 21:13:50 -0300

    aula 07/10

---
+++ aula 07.10.r
@@ -0,0 +1,23 @@
+#AULA 07/10
+
+#MAPA DE CALOR
+#a normal, M sem escala
+data (mtcars)
+scale(mtcars$mpg)
+mtcars2 = apply (mtcars, 2, scale)
+M = as.matrix(mtcars2)
+heatmap(M, Rowv = NA, Colv = NA)
+head(mtcars)
+a = as.matrix(mtcars)
+heatmap(a, col = heat.colors(256), Rowv = NA, Colv = NA)
+
+#TABLEPLOT
+install.packages("devtools")
+require (devtools)
+install_github("edwindj/ffbase", subdir = "pkg")
+install_github("mtennekes/tabplot")
+require(tabplot)
+tableplot(iris, sortCol = 1)
+tableplot(iris, sortCol = 2)
+tableplot(iris, sortCol = 3)
+tableplot(iris, sortCol = 4)
